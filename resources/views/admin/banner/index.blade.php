e4b2547b70d82d3f1e588ef40da82fe270063c42
Saw <saw@firstcom.com.sg>
2022-09-16 17:05:14 +0800

    update/banner_form

--- resources/views/admin/banner/index.blade.php
+++ resources/views/admin/banner/index.blade.php
@@ -5,51 +5,43 @@
 <div class="col-md-12">
     <div class="card mb-4">
         <div class="card-body">
-            <div class="card-title mb-3">Form Inputs Rounded</div>
-            <form>
-                <div class="row">
+            <div class="card-title mb-3">Banner Information</div>
+            <form action="{{ route('store.banner') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="row"></div>
                     <div class="col-md-6 form-group mb-3">
-                        <label for="firstName2">First name</label>
-                        <input class="form-control form-control-rounded" id="firstName2" type="text" placeholder="Enter your first name">
+                        <label for="BrideName">Bride Name</label>
+                        <input name="title_one" class="form-control " id="firstName2" type="text" placeholder="Enter Bride name">
                     </div>
                     <div class="col-md-6 form-group mb-3">
-                        <label for="lastName2">Last name</label>
-                        <input class="form-control form-control-rounded" id="lastName2" type="text" placeholder="Enter your last name">
+                        <label for="GroomName">Groom Name</label>
+                        <input name="title_two" class="form-control" id="lastName2" type="text" placeholder="Enter Groom name">
                     </div>
                     <div class="col-md-6 form-group mb-3">
-                        <label for="exampleInputEmail2">Email address</label>
-                        <input class="form-control form-control-rounded" id="exampleInputEmail2" type="email" placeholder="Enter email">
+                        <label for="Sign">Sign</label>
+                        <input name="sign" class="form-control" id="lastName2" type="text" placeholder="Enter Sign">
                     </div>
                     <div class="col-md-6 form-group mb-3">
-                        <label for="phone1">Phone</label>
-                        <input class="form-control form-control-rounded" id="phone1" placeholder="Enter phone">
+                        <label for="Description">Description</label>
+                        <input name="subtitle" class="form-control" id="lastName2" type="text" placeholder="Enter Description">
                     </div>
-                    <div class="col-md-6 form-group mb-3">
-                        <label for="credit2">Cradit card number</label>
-                        <input class="form-control form-control-rounded" id="credit2" placeholder="Card">
+                    <div class="col-md-12 form-group mb-3 p-50">
+                        <label for="Image">Image</label>
+                        <div class="custom-file">
+                            <input name="image" class="custom-file-input" id="image" type="file">
+                            <label class="custom-file-label" for="inputGroupFile02" aria-describedby="inputGroupFileAddon02">Choose file</label>
+                        </div>
+                        {{-- <div class="input-group-append"><span class="input-group-text" id="inputGroupFileAddon02">Upload</span></div> --}}
                     </div>
-                    <div class="col-md-6 form-group mb-3">
-                        <label for="website2">Website</label>
-                        <input class="form-control form-control-rounded" id="website2" placeholder="Web address">
-                    </div>
-                    <div class="col-md-6 form-group mb-3">
-                        <label for="picker3">Birth date</label>
-                        <input class="form-control form-control-rounded" id="picker3" placeholder="yyyy-mm-dd" name="dp">
-                    </div>
-                    <div class="col-md-6 form-group mb-3">
-                        <label for="picker1">Select</label>
-                        <select class="form-control form-control-rounded">
-                            <option>Option 1</option>
-                            <option>Option 1</option>
-                            <option>Option 1</option>
-                        </select>
-                    </div>
+                    
                     <div class="col-md-12">
-                        <button class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
+
+
 @endsection
