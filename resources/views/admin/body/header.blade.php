045d811930ca44a5f0b3fc057108f03dc1308bf3
Saw <saw@firstcom.com.sg>
2022-09-26 14:19:43 +0800

    update/icons

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -4,22 +4,26 @@
             <div></div>
             <div></div>
             <div></div>
-        </div><i class="i-Add-UserStar mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Todo"></i><i class="i-Speach-Bubble-3 mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Chat"></i><i class="i-Email mr-3 text-20 mobile-hide cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Inbox"></i><i class="i-Calendar-4 mr-3 mobile-hide text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Calendar"></i><i class="i-Checkout-Basket mobile-hide mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Calendar"></i>
+        </div>
+        <i class="fa-solid fa-id-card mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Todo"></i>
+        <i class="fa-solid fa-comment-dots mr-3 text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Chat"></i>
+        <i class="fa-solid fa-envelope mr-3 text-20 mobile-hide cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Inbox"></i>
+        <i class="fa-solid fa-calendar-days mr-3 mobile-hide text-20 cursor-pointer" data-toggle="tooltip" data-placement="top" title="" data-original-title="Calendar"></i>
     </div>
     <div class="header-part-right">
-        <!-- Full screen toggle--><i class="i-Full-Screen header-icon d-none d-sm-inline-block" data-fullscreen=""></i>
+        <!-- Full screen toggle--><i class="fa-solid fa-arrows-up-down-left-right header-icon d-none d-sm-inline-block" data-fullscreen=""></i>
         <!-- Grid menu Dropdown-->
-        <div class="dropdown dropleft"><i class="i-Safe-Box text-muted header-icon" id="dropdownMenuButton" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+        <div class="dropdown dropleft"><i class="fa-solid fa-ellipsis text-muted header-icon" id="dropdownMenuButton" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 <div class="menu-icon-grid">
+                        <a href="/" target="blank">
+                            <i class="fa-solid fa-house"></i> Home
+                        </a>
                     
-                    <a href="{{ route('user.logout')}}"><i class="i-Shop-4"></i> Logout</a>
-                    <a href="#">
-                        <i class="i-Library"></i> UI Kits</a>
-                        <a href="#"><i class="i-Drop"></i> Apps</a>
-                        <a href="#"><i class="i-File-Clipboard-File--Text"></i> Forms</a>
-                        <a href="#"><i class="i-Checked-User"></i> Sessions</a>
-                        <a href="#"><i class="i-Ambulance"></i> Support</a>
+                        <a href="{{ route('user.logout')}}">
+                            <i class="fa-solid fa-right-from-bracket"></i> Logout
+                        </a>
+                       
                     </div>
             </div>
         </div>
