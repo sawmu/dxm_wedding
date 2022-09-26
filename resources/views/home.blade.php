7755b137d73f999f58689da3b0b3e1c74284dc2a
Saw <saw@firstcom.com.sg>
2022-09-26 17:59:55 +0800

    delete/home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,26 +34,6 @@
         @include('layouts.body.footer')
         <!-- end of wpo-site-footer-section -->
 
-
-        <!-- color-switcher -->
-        {{-- <div class="color-switcher-wrap">
-            <div class="color-switcher-item">
-                <div class="color-toggle-btn">
-                    <i class="fa fa-cog"></i>
-                </div>
-                <ul id="switcher">
-                    <li class="btn btn1" id="Button1"></li>
-                    <li class="btn btn2" id="Button2"></li>
-                    <li class="btn btn3" id="Button3"></li>
-                    <li class="btn btn4" id="Button4"></li>
-                    <li class="btn btn5" id="Button5"></li>
-                    <li class="btn btn6" id="Button6"></li>
-                    <li class="btn btn7" id="Button7"></li>
-                    <li class="btn btn8" id="Button8"></li>
-                    <li class="btn btn9" id="Button9"></li>
-                </ul>
-            </div>
-        </div> --}}
     </div>
     <!-- end of page-wrapper -->
 
